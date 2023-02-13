49e9ed1d09e9ebeb33e2fea7ead76ea2200fe0fc
Alpha <alphagma@gmail.com>
2023-02-13 18:39:00 -0500

    More post methods

--- app/post_routes.py
+++ app/post_routes.py
@@ -12,6 +12,10 @@ router = APIRouter(
 )
 
 class BlogPost(BaseModel):
+    id: Optional[int] = Field(
+        description="Id of the post",
+        alias="id",
+    )
     title: str = Field(
         min_length=3,
         max_length=200,
@@ -36,3 +40,31 @@ def create_post(post: BlogPost):
     post.created_at = datetime.now()
     post_repository = PostRepository()
     return post_repository.create(post)
+
+@router.get("/{post_id}", response_model=BlogPost, status_code=200,
+    tags=["post"], summary="Get a post by id",
+    description="Get a post by id")
+def get_post(post_id: int):
+    post_repository = PostRepository()
+    return post_repository.get(post_id)
+
+@router.get("/", response_model=list[BlogPost], status_code=200,
+    tags=["post"], summary="Get all posts",
+    description="Get all posts")
+def get_all_posts():
+    post_repository = PostRepository()
+    return post_repository.get_all()
+
+@router.put("/{post_id}", response_model=BlogPost, status_code=200,
+    tags=["post"], summary="Update a post by id",
+    description="Update a post by id")
+def update_post(post_id: int, post: BlogPost):
+    post_repository = PostRepository()
+    return post_repository.update(post_id, post)
+
+@router.delete("/{post_id}", status_code=204,
+    tags=["post"], summary="Delete a post by id",
+    description="Delete a post by id")
+def delete_post(post_id: int):
+    post_repository = PostRepository()
+    post_repository.delete(post_id)
--- app/repositories/post.py
+++ app/repositories/post.py
@@ -1,8 +1,23 @@
 class PostRepository():
-    def __init__(self):
-        self.db = {}
-        self.db['posts'] = []
+    last_id = 0
+    db = {}
+    db['posts'] = {}
 
     def create(self, post):
-        self.db.get('posts').append(post)
+        PostRepository.last_id += 1
+        post.id = PostRepository.last_id
+        PostRepository.db.get('posts')[post.id] = post
         return post
+
+    def get(self, post_id):
+        return PostRepository.db.get('posts')[post_id]
+
+    def get_all(self):
+        return list(PostRepository.db.get('posts').values())
+
+    def update(self, post_id, post):
+        PostRepository.db.get('posts')[post_id] = post
+        return post
+
+    def delete(self, post_id):
+        del PostRepository.db.get('posts')[post_id]
